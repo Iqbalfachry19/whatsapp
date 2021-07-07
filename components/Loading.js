28faf0be16193c359477037345847d23c9fd9c87
iqbalfachry19 <ifachry@rocketmail.com>
2021-07-07 16:07:32 +0700

    fix

--- components/Loading.js
+++ components/Loading.js
@@ -4,7 +4,7 @@ function Loading() {
     return (
         <center style={{display:"grid",placeItems:"center", height:"100vh"}}>
             <div>
-                <Image style={{marginBottom:10}} src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" alt="whatsapp" width={200} height={200} />
+                <Image style={{marginBottom:10}} src="/1.png" alt="whatsapp" width={200} height={200} />
                 <Circle color="#3CBC28" size={60} />
                 </div> 
         </center>
