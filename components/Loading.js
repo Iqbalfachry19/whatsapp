b8a7766314f4c1edbfe4bde91fc517f27e154043
iqbalfachry19 <ifachry@rocketmail.com>
2021-07-07 15:27:24 +0700

    first commit

--- components/Loading.js
+++ components/Loading.js
@@ -1,10 +1,10 @@
 import {Circle} from "better-react-spinkit"
-
+import Image from "next/image"
 function Loading() {
     return (
         <center style={{display:"grid",placeItems:"center", height:"100vh"}}>
             <div>
-                <img style={{marginBottom:10}} src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" alt="" height={200} />
+                <Image style={{marginBottom:10}} src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" alt="" height={200} />
                 <Circle color="#3CBC28" size={60} />
                 </div> 
         </center>
